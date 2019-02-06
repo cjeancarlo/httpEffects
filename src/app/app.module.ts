8817b8ed22c8c37d179c7c0493038e7dd682f7a4
JeanCartaya <jeancarlo.cartaya@gmail.com>
2019-02-06 16:20:30 -0300

    final de la leccion 12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SharedModule } from './shared/shared/shared.module';
+import { UsersModule } from './users/users.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    SharedModule,
+    UsersModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map  } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+private readonly URL = 'https://reqres.in/api';
+
+constructor(private http: HttpClient ) { }
+
+getUsers( ) {
+  return this.http.get(`${ this.URL}/users?per_page=6`)
+  .pipe(
+    map( ( resp: any )  => resp.data )
+  );
+}
+
+}
